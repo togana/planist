26a5075d26f5a8aa36683631471fad29d81ed254
togana <o.togana@gmail.com>
2017-02-28 00:37:52 +0900

    fix routers

--- src/routers/users/index.js
+++ src/routers/users/index.js
@@ -1,24 +1,11 @@
 const uuidV4 = require('uuid/v4');
+const modules = require('../../models');
 
-// ダミーデータ(1以上の整数をidに持つuserデータ)
-const dummy = {
-  ids: [
-    'c591c386-5360-49a7-b9b6-c168d90b1d3b',
-    '80eed615-be37-4aeb-b533-3d0b7a9ee31c',
-  ],
-  results: {
-    'c591c386-5360-49a7-b9b6-c168d90b1d3b': {
-      name: 'user1',
-    },
-    '80eed615-be37-4aeb-b533-3d0b7a9ee31c': {
-      name: 'user2',
-    },
-  },
-};
+const userData = modules.users.find();
 
 // ユーザー一覧
 const list = (ctx) => {
-  ctx.body = dummy;
+  ctx.body = userData;
 };
 
 // ユーザー追加
@@ -26,8 +13,9 @@ const create = (ctx) => {
   if (ctx.request.body.name) {
     const id = uuidV4();
     // dummyの登録処理
-    dummy.ids.push(id);
-    dummy.results[id] = {
+    userData.result.push(id);
+    userData.entities.users[id] = {
+      id,
       name: ctx.request.body.name,
     };
     ctx.body = { result: true, id };
@@ -39,8 +27,8 @@ const create = (ctx) => {
 // ユーザー詳細
 const show = (ctx) => {
   const id = ctx.params.id;
-  if (dummy.ids.indexOf(id) !== -1) {
-    ctx.body = dummy.results[id];
+  if (userData.result.indexOf(id) !== -1) {
+    ctx.body = userData.entities.users[id];
   } else {
     ctx.body = {};
   }
